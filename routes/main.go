3433df4acf4059c97e3e565ee527801cb2f03b35
NingmengLLL <2283973222@qq.com>
2026-05-27 15:29:56 +0800

    fix abug

--- routes/main.go
+++ routes/main.go
@@ -8,7 +8,7 @@ import (
 
 func InitRoute() {
 
-	http.HandleFunc("/", controller.Category)
+	http.HandleFunc("/", controller.Index)
 	http.HandleFunc("/blog", controller.Index)
 	http.HandleFunc("/categories", controller.Category)
 	http.HandleFunc("/article", controller.Article)
